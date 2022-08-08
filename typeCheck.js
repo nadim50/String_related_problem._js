cc74807c0f2599d88585aaab0205398917b067c4
nadim50 <mdnadimmir50@gmail.com>
2022-08-08 19:45:48 +0600

    update

---
+++ typeCheck.js
@@ -0,0 +1,22 @@
+
+//// type of check
+const a = 34;
+const b = [1, 2, 3, 4, 5];
+const c = true;
+const d = 'Bangladesh';
+function add(a, b) {
+    return a + b;
+}
+
+const e = {
+    id: 13,
+    dep: 'cse',
+    college: 'ice'
+};
+
+console.log(typeof a);
+console.log(Array.isArray(b));
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
